92bd0a3e672588a6a5dd30bfb46caa55105c891d
yimian_liyangliang <liyangliang@yimian.com.cn>
2014-10-31 11:43:51 +0800

    ginpprof and example

---
+++ example.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/DeanThompson/ginpprof"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	// automatically add routers for net/http/pprof
+	// e.g. /debug/pprof, /debug/pprof/heap, etc.
+	ginpprof.Wrapper(router)
+
+	router.Run(":8080")
+}
